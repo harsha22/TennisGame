b8750c45a01a6e7c9153476cab291a233dcc6507
Sri Harish <test@L-MAA-25004247.paypalcorp.com>
2015-07-27 11:24:45 +0530

    Coded the getter function scoreAt

--- TennisGame.java
+++ TennisGame.java
@@ -11,7 +11,7 @@ public class TennisGame {
 	private static String DEUCE = "Deuce";
 	private static String ADVANTAGE = "Advantage";
 	private static String SEPARATOR  = ":";
-	
+
 	public TennisGame(String scoreString) {
 		firstPlayerScore = new ArrayList<Integer>();
 		secondPlayerScore = new ArrayList<Integer>();
@@ -19,10 +19,29 @@ public class TennisGame {
 	}
 
 	public String scoreAt(int index) {
-		return null;
+		int playerOneScore = firstPlayerScore.get(index);
+		int playerTwoScore = secondPlayerScore.get(index);
+		String displayValue = "";
+		boolean isWin = (playerOneScore > 3 || playerTwoScore > 3) && Math.abs(playerOneScore - playerTwoScore) == 2;
+		boolean isDeuce = (playerOneScore > 2 || playerTwoScore > 2) && playerOneScore  == playerTwoScore;
+		boolean isAdvantage = (playerOneScore > 2 || playerTwoScore > 2) && Math.abs(playerOneScore - playerTwoScore) == 1;
+		if (isWin) {
+			displayValue = playerInLead(playerOneScore, playerTwoScore) + " Wins";
+		} else if (isDeuce) {
+			displayValue = DEUCE;
+		} else if (isAdvantage) {
+			displayValue = ADVANTAGE + SEPARATOR + playerInLead(playerOneScore, playerTwoScore);
+		} else {
+			displayValue = scoreList[playerOneScore] + SEPARATOR + scoreList[playerTwoScore];
+		}
+		return displayValue;
+	}
+	
+	private String playerInLead(int player1score, int player2score) {
+		return player1score > player2score ? "Player One" : "Player Two"; 
 	}
 
 	private void calculateScore(String scoreString) {
-		
+
 	}
 }
