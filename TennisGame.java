8f3fcba7b694eef72c4d7784808147a59a01d5b4
Sri Harish Sridhar <mksharry@gmail.com>
2015-07-27 11:22:54 +0530

    Added class variables scoreList, DEUCE, ADVANTAGE and SEPARATOR

--- TennisGame.java
+++ TennisGame.java
@@ -7,7 +7,11 @@ public class TennisGame {
 
 	private List<Integer> firstPlayerScore;
 	private List<Integer> secondPlayerScore;
-
+	private static final int[] scoreList = {0, 15, 30, 40};
+	private static String DEUCE = "Deuce";
+	private static String ADVANTAGE = "Advantage";
+	private static String SEPARATOR  = ":";
+	
 	public TennisGame(String scoreString) {
 		firstPlayerScore = new ArrayList<Integer>();
 		secondPlayerScore = new ArrayList<Integer>();
