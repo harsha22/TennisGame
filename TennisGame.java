030da2fed214b87a3772d7ea97247e374d1f0bd2
unknown <test@L-MAA-25004061.paypalcorp.com>
2015-07-27 10:31:53 +0530

    Created Base Classes with function Stubs

---
+++ TennisGame.java
@@ -0,0 +1,15 @@
+package TennisGame;
+import java.util.List;
+public class TennisGame {
+
+	private List<Integer> firstPlayerScore;
+	private List<Integer> secondPlayerScore;
+	
+	public String scoreAt(int index) {
+	 return null;	
+	}
+	
+	public void calculateScore(String s) {
+		
+	}
+}
---
+++ TennisScore.java
@@ -0,0 +1,9 @@
+package TennisGame;
+
+public class TennisScore {
+	
+	public static void main(String[] args) {
+		//arg[0] is the input string
+		
+	}
+}
